8b8158eee227a75f53f9d1e3359857097d4423a8
baimamboukar <baimamboukar@gmail.com>
2021-12-17 21:33:15 +0100

    refactoring

---
+++ lib/controllers/postsController.dart
@@ -0,0 +1,11 @@
+import 'package:eureka_learn/models/models.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class PostsController extends StateNotifier<List<PostModel>> {
+  // ignore: unused_field
+  final Reader _read;
+  PostsController(this._read, List<PostModel> posts) : super([]);
+  List<PostModel> get feeds => state;
+
+  set data(List<PostModel> blogs) => state = blogs;
+}
