c184171d5cf9d4b051a141c44926092258870daf
baimamboukar <baimamboukar@gmail.com>
2021-12-20 21:59:38 +0100

    getQuestions parameters changed

--- lib/services/base_quiz_repository.dart
+++ lib/services/base_quiz_repository.dart
@@ -1,11 +1,11 @@
+import 'package:eureka_learn/models/models.dart';
 import 'package:eureka_learn/models/question_model.dart';
 import 'package:eureka_learn/enum/difficulty.dart';
 
 abstract class BaseQuizRepository {
   Future<List<Question>> getQuestions({
+    required Student student,
     int? numQuestions,
-    String? section,
-    String? level,
-    String? subject,
+    required String subject,
   });
 }
