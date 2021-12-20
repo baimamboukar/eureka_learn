bb1f6cda4b25a7abaf281e468779c37dd9152024
baimamboukar <baimamboukar@gmail.com>
2021-12-20 01:33:30 +0100

    commiter file improved

--- commit.sh
+++ commit.sh
@@ -1,77 +1,39 @@
-cd lib/controllers
-for file in *
-do
-  git add $file
-  git commit -m $1
-  git push
-done
-
-
-cd -
-cd lib/enum
-for file in *
-do
-  git add $file
-  git commit -m $1
-  git push
-done
-git push
-
-cd -
-cd lib/models
+cd lib/widgets
 for file in *
 do
   git add $file
   git commit -m $1
 done
-git push
 
 cd -
-cd lib/providers
+
+cd lib/utils
 for file in *
 do
   git add $file
   git commit -m $1
-  git push
 done
-git push
+
 
 cd -
+
+cd lib/extensions
+for file in *
+do
+  git add $file
+  git commit -m $1
+done
+
+
+cd -
+
 cd lib/screens
 for file in *
 do
   git add $file
   git commit -m $1
 done
-git push
 
 cd -
-cd lib/services
-for file in *
-do
-  git add $file
-  git commit -m $1
-done
-git push
-
-cd -
-cd lib/utils
-for file in *
-do
-  git add $file
-  git commit -m $1
-  git push
-done
-git push
-
-cd -
-cd lib/widgets
-for file in *
-do
-  git add $file
-  git commit -m $1
-  git push
-done
-git push
-
-
+git add main.dart
+git commit -m $1
