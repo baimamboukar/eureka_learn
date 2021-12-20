92e2cc8fed1c16b07079db361306a7a964c639dd
baimamboukar <baimamboukar@gmail.com>
2021-12-20 01:32:50 +0100

    fromdbposts

---
+++ lib/extensions/truncate.dart
@@ -0,0 +1,7 @@
+extension Truncate on String {
+  String truncate({required int max, String suffix = ''}) {
+    return length < max
+        ? this
+        : '${substring(0, substring(0, max - suffix.length).lastIndexOf(" "))}$suffix';
+  }
+}
