10f8422200a993a221eb91e3a2f9bc7bf4db84c8
baimamboukar <baimamboukar@gmail.com>
2021-12-22 16:29:32 +0100

    changing useProvider to ref.watch

--- lib/services/root.dart
+++ lib/services/root.dart
@@ -4,16 +4,15 @@ import 'package:eureka_learn/providers/database_providers.dart';
 import 'package:eureka_learn/screens/screens.dart';
 import 'package:eureka_learn/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Root extends HookWidget {
+class Root extends ConsumerWidget {
   const Root({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final user = useProvider(authStateProvider);
-    final database = useProvider(databaseProvider);
+  Widget build(BuildContext context, ScopedReader watch) {
+    final user = watch(authStateProvider);
+    final database = watch(databaseProvider);
     return user.when(
         loading: () => Scaffold(body: Loading()),
         error: (_, __) => Text("Something went wrong"),
