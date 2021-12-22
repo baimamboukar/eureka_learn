018c71ccefc87cfdeeaeb09671413abb6809e5ca
baimamboukar <baimamboukar@gmail.com>
2021-12-22 12:29:13 +0100

    adding sleep to test shimmer effect

--- lib/providers/feeds_provider.dart
+++ lib/providers/feeds_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eureka_learn/models/models.dart';
 import 'package:eureka_learn/providers/database_providers.dart';
