807577b60665d94064078ab2494ecdbf2b00d3b3
baimamboukar <baimamboukar@gmail.com>
2021-12-22 15:49:46 +0100

    adding from firebase questions provider

--- lib/providers/quizz_questions_provider.dart
+++ lib/providers/quizz_questions_provider.dart
@@ -1,4 +1,5 @@
 import 'package:eureka_learn/models/models.dart';
+import 'package:eureka_learn/providers/database_providers.dart';
 import 'package:eureka_learn/providers/providers.dart';
 import 'package:eureka_learn/services/quiz_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -10,3 +11,11 @@ final AutoDisposeFutureProvider<List<Question>>? quizQuestionsProvider =
       subject: ref.read(quizSubjectProvider).state,
       numQuestions: 4),
 );
+
+final questionsProvider = FutureProvider((ref) => ref
+    .read(databaseProvider)
+    .getQuestions(
+        numberOfQuestions: 3,
+        subject: "Biology",
+        difficulty: "medium",
+        type: "exam"));
