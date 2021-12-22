3e9bf65f6abfa79fd014a8c83b0ce6b9d37caad8
baimamboukar <baimamboukar@gmail.com>
2021-12-22 16:28:30 +0100

    adding limit and where on getquestions query

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -209,7 +209,12 @@ class Database {
       required String type}) async {
     List<Question> _questions = [];
     try {
-      await _firestore.collection("questions").get().then((questions) {
+      await _firestore
+          .collection("questions")
+          .where("subject", isEqualTo: subject)
+          .limit(numberOfQuestions)
+          .get()
+          .then((questions) {
         questions.docs.forEach(
             (question) => _questions.add(Question.fromMap(question.data())));
       });
