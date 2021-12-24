9e4e7454c615c7cd8c61a5572a61103db179166e
baimamboukar <baimamboukar@gmail.com>
2021-12-24 10:08:57 +0100

    changed to state providers

--- lib/providers/auth_providers.dart
+++ lib/providers/auth_providers.dart
@@ -3,12 +3,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:eureka_learn/services/services.dart';
 
-final firebaseAuthProvider = Provider<FirebaseAuth>((ref) {
+final firebaseAuthProvider = StateProvider<FirebaseAuth>((ref) {
   return FirebaseAuth.instance;
 });
-final authProvider = Provider<Authentication>((ref) {
-  return Authentication(ref.read(firebaseAuthProvider), ref.read);
+final authProvider = StateProvider<Authentication>((ref) {
+  return Authentication(ref.read(firebaseAuthProvider).state, ref.read);
 });
 
 final authStateProvider = StreamProvider.autoDispose<User?>(
-    (ref) => ref.watch(authProvider).authStateChanges);
+    (ref) => ref.watch(authProvider).state.authStateChanges);
