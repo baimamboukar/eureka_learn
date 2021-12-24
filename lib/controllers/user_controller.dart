ca72f07b1d2712a7dbbf3e5c651dcb72f402f572
baimamboukar <baimamboukar@gmail.com>
2021-12-24 17:29:06 +0100

    added refresh method to controller

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,10 +1,15 @@
 import 'package:eureka_learn/models/models.dart';
+import 'package:eureka_learn/providers/database_providers.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class StudentController extends StateNotifier<Student> {
   // ignore: unused_field
   final Reader _read;
+
   StudentController(this._read, Student student) : super(student);
   Student get student => state;
   set data(Student _student) => state = _student;
+  void refresh() {
+    _read(databaseProvider).getUser(state.id ?? "");
+  }
 }
