985af9c32fff14d1131aa0254851cfdff39e3a4b
baimamboukar <baimamboukar@gmail.com>
2021-12-28 01:29:09 +0100

    adding importance to notiff channels

--- lib/services/notifications.dart
+++ lib/services/notifications.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:awesome_notifications/android_foreground_service.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:eureka_learn/utils/utils.dart';
@@ -14,13 +15,27 @@ class NotificationService {
   void init() {
     _notiff.initialize(null, [
       NotificationChannel(
+          importance: NotificationImportance.High,
+          defaultPrivacy: NotificationPrivacy.Public,
+          criticalAlerts: true,
           channelShowBadge: true,
           playSound: true,
           channelKey: 'posts',
           channelName: 'Basic notifications',
           channelDescription: 'Notification channel for basic tests',
           defaultColor: Palette.primary,
-          ledColor: Colors.white)
+          ledColor: Colors.white),
+      NotificationChannel(
+          importance: NotificationImportance.High,
+          defaultPrivacy: NotificationPrivacy.Public,
+          criticalAlerts: true,
+          channelShowBadge: true,
+          playSound: true,
+          channelKey: 'hacktitudes',
+          channelName: 'Sending daily hacktitudes to students',
+          channelDescription: 'Notification channel for basic tests',
+          defaultColor: Palette.primary,
+          ledColor: Colors.white),
     ]);
   }
 
@@ -39,9 +54,12 @@ class NotificationService {
       _notiff.createNotification(
           content: NotificationContent(
             showWhen: true,
+            fullScreenIntent: true,
+            category: NotificationCategory.Social,
+            wakeUpScreen: true,
             displayOnBackground: true,
             displayOnForeground: true,
-            ticker: "Post alert",
+            ticker: "tick",
             id: Random().nextInt(100000),
             channelKey: channel,
             title: title,
@@ -61,6 +79,23 @@ class NotificationService {
                 key: "beta")
           ]).then((response) async {});
     });
+  }
+
+  static void foreGround() {
+    AndroidForegroundService.startForeground(
+        content: NotificationContent(
+            id: 2341234,
+            body: 'Service is running!',
+            title: 'Android Foreground Service',
+            channelKey: 'basic_channel',
+            bigPicture:
+                'https://firebasestorage.googleapis.com/v0/b/eurekalearn-d63d4.appspot.com/o/eureka.png?alt=media&token=9d806524-caa7-4d72-a006-e4ae6e578e0b',
+            notificationLayout: NotificationLayout.BigPicture,
+            category: NotificationCategory.Service),
+        actionButtons: [
+          NotificationActionButton(
+              key: 'SHOW_SERVICE_DETAILS', label: 'Show details')
+        ]);
   }
 }
 
