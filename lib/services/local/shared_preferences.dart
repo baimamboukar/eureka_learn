ad8a0aa2174c36e7a68ac18c6b8b11c1a23e53d2
baimamboukar <baimamboukar@gmail.com>
2022-01-22 11:08:15 +0100

    re-modyfing modes

--- lib/services/local/shared_preferences.dart
+++ lib/services/local/shared_preferences.dart
@@ -1,8 +1,13 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesServices {
-  static Future<void> theme(bool isDarkOn) async {
+  static Future<void> setMode(bool darkMode) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('theme', isDarkOn);
+    await prefs.setBool('theme', darkMode);
+  }
+
+  static Future<bool> getMode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('theme') ?? false;
   }
 }
