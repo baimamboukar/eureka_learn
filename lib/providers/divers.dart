899600266a4c51dbed8d28893a3a4e88ca50254b
baimamboukar <baimamboukar@gmail.com>
2022-01-22 11:08:27 +0100

    adding provider

--- lib/providers/divers.dart
+++ lib/providers/divers.dart
@@ -1,1 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+final themeProvider = FutureProvider<bool>((ref) async {
+  return await SharedPreferences.getInstance().then((prefs) {
+    return prefs.getBool('darkMode') ?? false;
+  });
+});
