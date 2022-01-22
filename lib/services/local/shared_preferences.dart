36bfb0a26856acd2c846041d36cd192f0bf56560
baimamboukar <baimamboukar@gmail.com>
2022-01-22 11:42:02 +0100

    darkmode prefs

--- lib/services/local/shared_preferences.dart
+++ lib/services/local/shared_preferences.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesServices {
@@ -9,5 +10,27 @@ class SharedPreferencesServices {
   static Future<bool> getMode() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getBool('theme') ?? false;
+  }
+}
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError();
+});
+final sharedUtilityProvider = Provider<SharedUtility>((ref) {
+  final _sharedPrefs = ref.watch(sharedPreferencesProvider);
+  return SharedUtility(sharedPreferences: _sharedPrefs);
+});
+
+class SharedUtility {
+  SharedUtility({
+    required this.sharedPreferences,
+  });
+  final SharedPreferences sharedPreferences;
+  bool isDarkModeEnabled() {
+    return sharedPreferences.getBool('isDarkModeEnabled') ?? false;
+  }
+
+  Future<bool> setDarkModeEnabled(bool value) async {
+    return await sharedPreferences.setBool('isDarkModeEnabled', value);
   }
 }
