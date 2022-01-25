2172e0d64f54e8f4df0048d4136fde778aa5dc8f
baimamboukar <baimamboukar@gmail.com>
2022-01-25 09:26:53 +0100

    warning fixed in profilr

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,7 +3,6 @@ import 'package:eureka_learn/providers/database_providers.dart';
 import 'package:eureka_learn/providers/user_provider.dart';
 import 'package:eureka_learn/utils/utils.dart';
 import 'package:eureka_learn/widgets/widgets.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
