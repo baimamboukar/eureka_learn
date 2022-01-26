820b9d154883d64d71ff4d0adbf7596797c57bc8
baimamboukar <baimamboukar@gmail.com>
2022-01-26 23:01:56 +0100

    fixed shimmer pb

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,13 +2,10 @@ import 'package:eureka_learn/models/models.dart';
 import 'package:eureka_learn/providers/database_providers.dart';
 import 'package:eureka_learn/providers/providers.dart';
 import 'package:eureka_learn/services/notifications.dart';
-import 'package:eureka_learn/utils/screen.dart';
 import 'package:eureka_learn/utils/utils.dart';
 import 'package:eureka_learn/widgets/widgets.dart';
-import 'package:eureka_learn/widgets/tips_banner.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_shimmer/flutter_shimmer.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:iconsax/iconsax.dart';
@@ -23,10 +20,7 @@ class NewsFeed extends HookWidget {
     WidgetsBinding.instance!.addPostFrameCallback((timeStamp) async {
       await database.getUserFeeds();
     });
-    final user = useProvider(studentControllerProvider.notifier);
-    final notiff = useProvider(notificationsProvider);
     final feedz = useProvider(feedsProvider);
-    List<PostModel> feeds = useProvider(postsControllerProvider.notifier).feeds;
 
     return RefreshIndicator(
       onRefresh: () async {
