03201dad4f9e9b89049f650c1ab77635cfb199ee
baimamboukar <baimamboukar@gmail.com>
2022-01-27 08:58:53 +0100

    mesages gen

--- lib/generated/intl/messages_fr.dart
+++ lib/generated/intl/messages_fr.dart
@@ -21,5 +21,8 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'fr';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "moto":
+            MessageLookupByLibrary.simpleMessage("Hack ton success maintenant!")
+      };
 }
