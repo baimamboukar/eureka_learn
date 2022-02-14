64aa60f8c709102e305b8cf5fef64945769d204b
baimamboukar <baimamboukar@gmail.com>
2022-02-14 19:50:32 +0100

    adding animations

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'package:eureka_learn/models/models.dart';
 import 'package:eureka_learn/providers/database_providers.dart';
 import 'package:eureka_learn/providers/providers.dart';
-import 'package:eureka_learn/services/notifications.dart';
 import 'package:eureka_learn/utils/utils.dart';
 import 'package:eureka_learn/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +8,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_shimmer/flutter_shimmer.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:lottie/lottie.dart';
 
 class NewsFeed extends HookWidget {
   const NewsFeed({Key? key}) : super(key: key);
@@ -66,17 +66,7 @@ class NewsFeed extends HookWidget {
             },
             loading: () {
               return SliverToBoxAdapter(
-                  child: Column(
-                children: [
-                  for (var i = 1; i <= 10; i++)
-                    ListTileShimmer(
-                      isRectBox: true,
-                      hasBottomBox: true,
-                      hasCustomColors: true,
-                      colors: [Palette.primary, Palette.secondary],
-                    )
-                ],
-              ));
+                  child: LottieBuilder.asset("loadfeed.json"));
             },
             error: (err, stack) =>
                 SliverToBoxAdapter(child: Center(child: Text(err.toString()))))
