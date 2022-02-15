1bf1ed15b2987442e60bb65e4325a8df64fcb274
baimamboukar <baimamboukar@gmail.com>
2022-02-15 22:54:01 +0100

    fixing linter errors

--- lib/services/base_quiz_repository.dart
+++ lib/services/base_quiz_repository.dart
@@ -1,6 +1,4 @@
 import 'package:eureka_learn/models/models.dart';
-import 'package:eureka_learn/models/question_model.dart';
-import 'package:eureka_learn/enum/difficulty.dart';
 
 abstract class BaseQuizRepository {
   Future<List<Question>> getQuestions({
