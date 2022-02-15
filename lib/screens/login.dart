3734fe8d0a20ef01e729a4b8afff64cb0bbbc12a
baimamboukar <baimamboukar@gmail.com>
2022-02-15 22:53:38 +0100

    fixing linter errors

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,7 +4,6 @@ import 'package:eureka_learn/screens/screens.dart';
 import 'package:eureka_learn/utils/utils.dart';
 import 'package:eureka_learn/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:line_icons/line_icons.dart';
