af80726420b1bbd6dd8285470d7482a01e3df19d
baimamboukar <baimamboukar@gmail.com>
2022-02-15 15:51:56 +0100

    done

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -7,11 +7,10 @@ import 'package:eureka_learn/screens/screens.dart';
 import 'package:eureka_learn/utils/utils.dart';
 import 'package:eureka_learn/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:iconsax/iconsax.dart';
+import 'package:iconsax/iconsax.dart';  
 import 'package:line_icons/line_icons.dart';
 
 final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
