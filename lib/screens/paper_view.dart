8a5146655e669f15ce3504cf791ba9a43d9f1c13
baimamboukar <baimamboukar@gmail.com>
2022-02-16 17:53:43 +0100

    added cached pdf view

--- lib/screens/paper_view.dart
+++ lib/screens/paper_view.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+//import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 
 class PaperView extends StatelessWidget {
   const PaperView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final PdfViewerController controller = PdfViewerController();
+    //final PdfViewerController controller = PdfViewerController();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Document"),
       ),
       body: Container(
-          child: SfPdfViewer.network(
-              "https://firebasestorage.googleapis.com/v0/b/eurekalearn-d63d4.appspot.com/o/documents%2F0750-AGEOGRAPHY3-1.pdf?alt=media&token=81cf5285-cbe0-4fb6-bdf2-11829baee7bb",
-              interactionMode: PdfInteractionMode.pan,
-              controller: controller,
-              onDocumentLoaded: (PdfDocumentLoadedDetails load) {
-        showAboutDialog(
-            context: context, children: [Text("${load.document.template}")]);
-      }, pageLayoutMode: PdfPageLayoutMode.continuous)),
+          child: PDF().cachedFromUrl(
+        'https://firebasestorage.googleapis.com/v0/b/eurekalearn-d63d4.appspot.com/o/documents%2FAL%20ECONS%20P2%202020-1.pdf?alt=media&token=92dfee45-0ed6-4c92-a6ce-365d1dd494e2',
+        placeholder: (progress) => Center(child: Text('$progress %')),
+        errorWidget: (error) => Center(child: Text(error.toString())),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          controller.searchText("GCE");
+          // controller.searchText("GCE");
         },
         child: Icon(Icons.file_download),
       ),
