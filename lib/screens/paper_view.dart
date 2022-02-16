b9422392b29b8cf967f5ea5afddda91ee217bd87
baimamboukar <baimamboukar@gmail.com>
2022-02-16 17:33:14 +0100

    optimizing pdf

--- lib/screens/paper_view.dart
+++ lib/screens/paper_view.dart
@@ -6,16 +6,27 @@ class PaperView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final PdfViewerController controller = PdfViewerController();
+
     return Scaffold(
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Container(
-              child: SfPdfViewer.network(
-                  "https://firebasestorage.googleapis.com/v0/b/eurekalearn-d63d4.appspot.com/o/documents%2Fpaper.pdf?alt=media&token=a6e86c4c-c206-466e-b91b-515f03680de0",
-                  pageLayoutMode: PdfPageLayoutMode.single))
-        ],
-      )),
+      appBar: AppBar(
+        title: const Text("Document"),
+      ),
+      body: Container(
+          child: SfPdfViewer.network(
+              "https://firebasestorage.googleapis.com/v0/b/eurekalearn-d63d4.appspot.com/o/documents%2F0750-AGEOGRAPHY3-1.pdf?alt=media&token=81cf5285-cbe0-4fb6-bdf2-11829baee7bb",
+              interactionMode: PdfInteractionMode.pan,
+              controller: controller,
+              onDocumentLoaded: (PdfDocumentLoadedDetails load) {
+        showAboutDialog(
+            context: context, children: [Text("${load.document.template}")]);
+      }, pageLayoutMode: PdfPageLayoutMode.continuous)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.searchText("GCE");
+        },
+        child: Icon(Icons.file_download),
+      ),
     );
   }
 }
