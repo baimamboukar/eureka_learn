a6318bb7374e1e9e16dbb3030ef3174f7e70768f
baimamboukar <baimamboukar@gmail.com>
2022-02-17 16:28:44 +0100

    filtering to exact paper

--- lib/screens/paper_view.dart
+++ lib/screens/paper_view.dart
@@ -1,26 +1,38 @@
 import 'package:eureka_learn/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class PaperView extends StatelessWidget {
+final failedProvider = StateProvider<bool>((ref) => false);
+
+class PaperView extends HookWidget {
   final PaperModel paper;
   const PaperView({Key? key, required this.paper}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final failed = useProvider(failedProvider);
     final PdfViewerController controller = PdfViewerController();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Document"),
       ),
-      body: Container(
-          child: SfPdfViewer.network(paper.link,
-              interactionMode: PdfInteractionMode.pan, controller: controller,
-              onDocumentLoaded: (PdfDocumentLoadedDetails load) {
-        showAboutDialog(
-            context: context, children: [Text("${load.document.template}")]);
-      }, pageLayoutMode: PdfPageLayoutMode.single)),
+      body: failed.state
+          ? LottieBuilder.asset("assets/animations/nothing.json")
+          : Container(
+              child: SfPdfViewer.network(paper.link,
+                  interactionMode: PdfInteractionMode.pan,
+                  controller: controller,
+                  onDocumentLoadFailed: (PdfDocumentLoadFailedDetails) {
+              failed.state = true;
+            }, onDocumentLoaded: (PdfDocumentLoadedDetails load) {
+              showAboutDialog(
+                  context: context,
+                  children: [Text("${load.document.template}")]);
+            }, pageLayoutMode: PdfPageLayoutMode.single)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           controller.searchText("GCE");
