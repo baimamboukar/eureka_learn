63dd47a73c3c724948a93a99a248b327a0e5e93a
baimamboukar <baimamboukar@gmail.com>
2022-02-17 18:35:29 +0100

    redesign icons paper

--- lib/widgets/paper.dart
+++ lib/widgets/paper.dart
@@ -1,4 +1,3 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:eureka_learn/models/paper_model.dart';
 import 'package:eureka_learn/screens/screens.dart';
 import 'package:eureka_learn/utils/utils.dart';
@@ -23,59 +22,62 @@ class Paper extends HookWidget {
               paper: model,
             ));
       },
-      child: FadeInDown(
-        child: Align(
-            heightFactor: 0.65,
-            child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(
-                          24.0,
-                        ),
-                        topRight: Radius.circular(24.0)),
-                    side: BorderSide(color: Colors.black12, width: 2.20)),
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: ListTile(
-                    leading: Icon(LineIcons.pdfFileAlt,
-                        color: Palette.error, size: 42.0),
-                    title: Text("${model.type}",
-                        overflow: TextOverflow.ellipsis,
-                        style: Styles.designText(
-                            bold: true,
-                            size: 16.0,
-                            color: Palette.primary.withOpacity(0.75))),
-                    subtitle: Text("${model.from}"),
-                    trailing: DropdownButton<String>(
-                      icon: Icon(LineIcons.download),
-                      underline: const SizedBox.shrink(),
-                      items: <String>[
-                        "Paper",
-                        "Answer",
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Row(
-                            children: [
-                              Icon(LineIcons.download),
-                              Text(
-                                value,
-                                style: TextStyle(color: Colors.black),
-                              ),
-                            ],
-                          ),
-                        );
-                      }).toList(),
-                      hint: Text(
-                        "Download",
-                      ),
-                      onChanged: (String? currentValue) {
-                        value.state = currentValue;
-                      },
+      child: Align(
+          heightFactor: 0.65,
+          child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(
+                      24.0,
                     ),
+                    topRight: Radius.circular(24.0)),
+                side: BorderSide(color: Palette.primary, width: 1.50),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: ListTile(
+                  leading: Image.asset("assets/icons/png/pdf.svg",
+                      height: 50, width: 50, fit: BoxFit.cover),
+                  title: Text("${model.type}",
+                      overflow: TextOverflow.ellipsis,
+                      style: Styles.designText(
+                          bold: true,
+                          size: 16.0,
+                          color: Palette.primary.withOpacity(0.75))),
+                  subtitle: Text(
+                    "${model.from}",
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
                   ),
-                ))),
-      ),
+                  trailing: DropdownButton<String>(
+                    icon: Icon(LineIcons.download),
+                    underline: const SizedBox.shrink(),
+                    items: <String>[
+                      "Paper",
+                      "Answer",
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Row(
+                          children: [
+                            Icon(LineIcons.download),
+                            Text(
+                              value,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                    hint: Text(
+                      "Download",
+                    ),
+                    onChanged: (String? currentValue) {
+                      value.state = currentValue;
+                    },
+                  ),
+                ),
+              ))),
     );
   }
 }
