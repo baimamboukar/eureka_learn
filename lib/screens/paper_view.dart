75677201c9fe932073f18b0134cece7871499000
baimamboukar <baimamboukar@gmail.com>
2022-02-17 16:02:40 +0100

    with paper update

--- lib/screens/paper_view.dart
+++ lib/screens/paper_view.dart
@@ -1,8 +1,10 @@
+import 'package:eureka_learn/models/models.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class PaperView extends StatelessWidget {
-  const PaperView({Key? key}) : super(key: key);
+  final PaperModel paper;
+  const PaperView({Key? key, required this.paper}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +15,8 @@ class PaperView extends StatelessWidget {
         title: const Text("Document"),
       ),
       body: Container(
-          child: SfPdfViewer.network(
-              "https://firebasestorage.googleapis.com/v0/b/eurekalearn-d63d4.appspot.com/o/documents%2F0750-AGEOGRAPHY3-1.pdf?alt=media&token=81cf5285-cbe0-4fb6-bdf2-11829baee7bb",
-              interactionMode: PdfInteractionMode.pan,
-              controller: controller,
+          child: SfPdfViewer.network(paper.link,
+              interactionMode: PdfInteractionMode.pan, controller: controller,
               onDocumentLoaded: (PdfDocumentLoadedDetails load) {
         showAboutDialog(
             context: context, children: [Text("${load.document.template}")]);
