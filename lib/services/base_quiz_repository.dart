cd009c6d4e602451a6cb38066c3d38dced7539fe
baimamboukar <baimamboukar@gmail.com>
2022-02-21 14:30:41 +0100

    adding query parameters

--- lib/services/base_quiz_repository.dart
+++ lib/services/base_quiz_repository.dart
@@ -1,9 +1,10 @@
 import 'package:eureka_learn/models/models.dart';
 
 abstract class BaseQuizRepository {
-  Future<List<Question>> getQuestions({
-    required Student student,
-    int? numQuestions,
-    required String subject,
-  });
+  Future<List<Question>> getQuestions(
+      {required Student student,
+      int? numQuestions = 10,
+      required String subject,
+      required String level,
+      String? topic});
 }
