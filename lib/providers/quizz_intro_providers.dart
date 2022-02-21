c8fcbd90caf9f18735fcc1fb1ccf30dad419dd03
baimamboukar <baimamboukar@gmail.com>
2022-02-21 15:21:15 +0100

    fixing provider erros

--- lib/providers/quizz_intro_providers.dart
+++ lib/providers/quizz_intro_providers.dart
@@ -3,4 +3,4 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 final quizzTypeProvider = StateProvider<String>((ref) => "standard");
 final difficultyProvider = StateProvider<String>((ref) => "medium");
 final numberOfQuestionsProvider = StateProvider<int>((ref) => 10);
-final quizzTopicProvider = StateProvider<String>((ref) => "");
+final quizzTopicProvider = StateProvider<String>((ref) => "novice");
