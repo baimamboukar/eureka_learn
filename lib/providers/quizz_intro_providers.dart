7efe109ea15ce729cfc55898cd4fde55f19bdb43
baimamboukar <baimamboukar@gmail.com>
2022-02-21 15:01:49 +0100

    proficient to medium

--- lib/providers/quizz_intro_providers.dart
+++ lib/providers/quizz_intro_providers.dart
@@ -1,6 +1,6 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final quizzTypeProvider = StateProvider<String>((ref) => "standard");
-final difficultyProvider = StateProvider<String>((ref) => "novice");
+final difficultyProvider = StateProvider<String>((ref) => "medium");
 final numberOfQuestionsProvider = StateProvider<int>((ref) => 10);
 final quizzTopicProvider = StateProvider<String>((ref) => "");
