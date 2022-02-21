dfbe4d68940ada30660f664f1b931a5e32c44521
baimamboukar <baimamboukar@gmail.com>
2022-02-21 15:57:38 +0100

    quizz repo done

--- lib/services/quiz_repository.dart
+++ lib/services/quiz_repository.dart
@@ -17,21 +17,33 @@ class QuizRepository extends BaseQuizRepository {
   @override
   Future<List<Question>> getQuestions(
       {required Student student,
-      int? numQuestions ,
+      int? numQuestions,
       required String subject,
       required String level,
       String? topic}) async {
     try {
+      print(
+          "mapping url:http://intelliquizz.herokuapp.com/$subject/$level/$numQuestions");
       final response = await _read(dioProvider).get(
-          'http://intelliquizz.herokuapp.com/$subject/$level/$numQuestions',
-          onReceiveProgress: (x, y) {});
+          "http://intelliquizz.herokuapp.com/$subject/$level/$numQuestions",
+          onReceiveProgress: (x, y) {
+        print("progress: $x / $y");
+      });
+      print("response data index: ${response.data['data']}");
 
       if (response.statusCode == 200) {
-        final data = Map<String, dynamic>.from(response.data);
-        final results = List<Map<String, dynamic>>.from(data["data"] ?? []);
+        // final data = Map<String, dynamic>.from(response.data);
+        final results =
+            List<Map<String, dynamic>>.from(response.data['data'] ?? []);
+        print("quizz set: $results");
         if (results.isNotEmpty) {
+          print("returned data");
+          print(results.map((e) => Question.fromMap(e)).toList());
           return results.map((e) => Question.fromMap(e)).toList();
         }
+      }
+      if (response.statusCode == 500) {
+        print("500 error");
       }
       return [];
     } on DioError catch (err) {
