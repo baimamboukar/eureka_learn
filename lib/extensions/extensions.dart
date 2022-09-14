6a70df1b55d17a64c7942ec97515bcb663a94716
baimamboukar <baimamboukar@gmail.com>
2022-09-14 14:29:11 +0100

    [eueureklean]  🚀

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,14 @@
+/* 
+  ✨✨✨ SNAG SPEED BUSINESS ✨✨✨
+  
+⛳ AUTHOR: BAIMAM BOUKAR JJ
+⛳ CONTACTS: baimamboukar@gmail.com | (+237)690535759
+
+🎯 BRIEF ==> This is a dart exporter file. It exports all extensions files
+
+⚽ USAGE ==> import `extensions.dart`, this will automatically import all extension files, intead of importing one by one and  manually
+*/
+
+export 'loc.dart';
+export 'router.dart';
+export 'screen.dart';
