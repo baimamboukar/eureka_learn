98842b18f3266ccd2b296f44de197f94b56c746e
baimamboukar <baimamboukar@gmail.com>
2022-09-14 14:29:11 +0100

    [eueureklean]  🚀

---
+++ lib/extensions/loc.dart
@@ -0,0 +1,18 @@
+/* 
+  ✨✨✨ SNAG SPEED BUSINESS ✨✨✨
+  
+⛳ AUTHOR: BAIMAM BOUKAR JJ
+⛳ CONTACTS: baimamboukar@gmail.com | (+237)690535759
+
+🎯 BRIEF ==> This file contains an dart extension to the `BuildContext`.
+It simplifies how you can access to localized messages
+
+⚽ USAGE ==> use context.loc.anyLocalizedMessage instead of Applocalizations.of(context)!.anyLocalizedMessage
+*/
+
+// import 'package:flutter/material.dart' show BuildContext;
+//     show AppLocalizations;
+
+// extension Localization on BuildContext {
+//   AppLocalizations get loc => AppLocalizations.of(this)!;
+// }
